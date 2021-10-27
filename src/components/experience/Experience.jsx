ab6f8c899a9593a3bff5a627c148b02a8d5415c6
adamhkb <adamhamzahkb@hotmail.com>
2021-10-27 14:31:13 +0800

    first commit

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -8,9 +8,9 @@ export default function Experience() {
         <h2><u>Machine Learning Engineer</u></h2>
         <p>
             <li>Led a group of engineers in developing and deploying artificial intelligence systems.</li>
-            <li>Research existing papers on computer vision, enhance existing open-source models and incorporated
-            novel techniques and create new algorithms from scratch.</li>
-            <li>Perform statistical analysis and document the impact of machine learning models.</li>
+            <li>Researched existing papers on computer vision, enhanced existing open-source models and incorporated
+            novel techniques and created new algorithms from scratch.</li>
+            <li>Performed statistical analysis and documented the impact of machine learning models.</li>
             <li>Built and Developed a Facial Recognition System</li> 
         </p>
         <span>&nbsp;</span>  
@@ -19,7 +19,7 @@ export default function Experience() {
         <h2><u>BEng Electronic Engineering with Management</u></h2>
         <p>
             <li>Achieved a 2:1 overall grade.</li>
-            <li>Wrote a dissertation on 'Eye Contact Detection using Convolutional Neural Networks'</li>
+            <li>Wrote a dissertation on 'Eye Contact Detection using Convolutional Neural Networks'.</li>
             <li>Key course modules:</li>
                 <ul>
                     <p>
